b80595e5e303f68be4f707810a35014034c79dcd
gali-lee <taekyu32@gmail.com>
2020-10-26 15:48:01 +0900

    다시 시작
    ㅋ

--- src/main/webapp/find-dog/src/App.js
+++ src/main/webapp/find-dog/src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import Header from './components/Header';
+import { Route } from 'react-router-dom';
+import Board1 from './pages/Board1/Board1';
+import Board2 from './pages/Board2/Board2';
+import Board3 from './pages/Board3/Board3';
+
+// 건들지 마시오!
 
 function App() {
   return (
     <div>
-123123123
+      <Header />
+      <Route path="/board1" exact={true} component={Board1} />
+      <Route path="/board2" exact={true} component={Board2} />
+      <Route path="/board3" exact={true} component={Board3} />
+
     </div>
   );
 }
